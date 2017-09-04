c3f86d257030a9a70e2796fc580b46192013f134
Annie <eminimini11@daum.et>
2017-09-04 09:22:11 +0900

    delete codes

--- app.py
+++ app.py
@@ -1,42 +1,25 @@
-from flask import Flask, render_template, request
-from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.script import Manager
-
-
-class NameForm(Form):
-    name = StringField('What is you name?', validators=[Required()])
-    subnit = SubmitField('Submit')
+from flask import Flask, render_template
+from flask_script import Manager
+# from flask import Flask
 
 
 app = Flask(__name__)
 manager = Manager(app)
-bootstrap = Bootstrap(app)
 
 
-@app.route('/result', methods=['GET', 'POST'])
-def result():
-    if request.method == 'POST':
-        result = request.form
-    return render_template('index.html', result=result)
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hells, %s!<h1/>' % name
 
 
-@app.route('/')
+@app.route('/index/')
 def index():
-    return render_template('index.html')
-
-
-@app.route('/user')
-def user():
-    return render_template('user.html')
+    return render_template('/index.html')
 
 
 @app.route('/user/hello')
 def hello():
-    user_name = request.args.get('user_name', '')
-    user_name = user_name + ", Welcom"
+    user_name = user + ", Welcom"
     return render_template('hello.html', user_name=user_name)
 
 
